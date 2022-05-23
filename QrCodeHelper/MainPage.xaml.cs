d5c531520df7202c7eeb480a3cdeb10ed10196d6
金俊成 <tkezyo@live.com>
2022-05-23 11:03:46 +0800

    添加项目文件。

---
+++ QrCodeHelper/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Media;
+using QrCodeHelper.ViewModels;
+
+namespace QrCodeHelper;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        var result = await PickAndShow(PickOptions.Images);
+
+        if (result is null)
+            return;
+
+        image.Source = result.FullPath;
+        var stream = System.IO.File.Open(result.FullPath, System.IO.FileMode.Open);
+        using (var skiaImage = SkiaSharp.SKBitmap.Decode(stream))
+        {
+            var skiaReader = new ZXing.SkiaSharp.BarcodeReader();
+            var skiaResult = skiaReader.Decode(skiaImage);
+            qrResult.Text = skiaResult?.Text ?? "未识别到结果！";
+        }
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    async Task<FileResult> PickAndShow(PickOptions options)
+    {
+        try
+        {
+            var result = await FilePicker.PickAsync(options);
+
+            return result;
+        }
+        catch
+        {
+            // The user canceled or something went wrong
+        }
+
+        return null;
+    }
+}
+
---
+++ QrCodeHelper/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using QrCodeHelper.ViewModels;
+
+namespace QrCodeHelper;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+               .ConfigureEssentials();
+
+        builder.Services.AddTransient(typeof(MainPage));
+        builder.Services.AddTransient(typeof(MainViewModel));
+
+
+
+        return builder.Build();
+    }
+}
